5833066a5136fc4ca33cafbcf94de495bc02329c
rahulannaldas0406 <rahulannaldas2006@gmail.com>
2024-10-06 11:50:18 +0530

    Update numberguessgame.c
    Extend the number guess to a game

--- numberguessgame.c
+++ numberguessgame.c
@@ -5,15 +5,18 @@ int main() {
   //Write your code here. 
 int number,guess,chance=0,x=1;
 int inputType;
+ printf("number",inputType);
 scanf("%d",&inputType);
 if (inputType == 1) {
+ printf("next",number);
     scanf("%d", &number);
 } 
  
 while(1){
+ printf("guess",guess);
   scanf("%d",&guess);
   chance++;
-  if(guess<number)
+  if(number>guess)
   printf("===>Number to be guessed is higher than %d\n",guess);
 else if(number<guess)
   printf("===>Number to be guessed is lesser than %d\n",guess);
@@ -22,7 +25,7 @@ else if(number==guess)
 printf("===>Guessed correctly the number %d in %d chances",number,chance);
 break;
 }
-if(chance==20)
+if(chance==2)
 {
 printf("===>You were not able to guess %d in 20 chances",number);
  break;
